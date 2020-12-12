4d1d284f0f24093f6f7588f18b743889752de0ad
shunppe <70781900+shunppe@users.noreply.github.com>
2020-12-13 02:48:57 +0900

    completed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 {
-  function createColum(col) {
+  function createColumn(col) {
     const source = [];
     for(let i = 0;i < 15;i++ ) {
         source[i] = i + 1 * col;
@@ -16,39 +16,36 @@
 
   }
 
-　const columns = [];
+  function createColumns() {
+  　const columns = [];
+    for(let i = 0;i < 5;i ++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
+  }
 
-  columns[0] = createColum(0);
-  columns[1] = createColum(1);
-  columns[2] = createColum(2);
-  columns[3] = createColum(3);
-  columns[4] = createColum(4);
-  columns[2][2] = 'FREE';
+  function renderBingo(columns) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
   
-
-    // console.table(columns);
-
-  const bingo = [];
-    for (let row = 0; row < 5;row++) {
-    bingo[row] = [];
-        for (let col = 0;col < 5;col++) {
-            
-            bingo[row][col] = columns[col][row];
-        }
-
-
-  }
-  console.table(bingo);
-
-  const tr = document.createElement('tr');
-
-  for (let col = 0;col < 5;col++) {
-    const td = document.createElement('td');
-    td.textContent = bingo[0][col];
-    tr.appendChild(td);
-  }
-
-  document.querySelector('tbody').appendChild(tr);
-
+  renderBingo(columns);
 
 }
+
+
+
+
+
+
+
